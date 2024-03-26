651aeb6f666f8490ca82ad3fa158a921cadf2040
MeskourLhoussaine <127257257+MeskourLhoussaine@users.noreply.github.com>
2024-03-26 13:48:51 +0000

    add

---
+++ amanPay/src/main/java/ma/project/amanpya/AmanPyaApplication.java
@@ -0,0 +1,13 @@
+package ma.project.amanpya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmanPyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmanPyaApplication.class, args);
+    }
+
+}
---
+++ amanPay/src/test/java/ma/project/amanpya/AmanPyaApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.project.amanpya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmanPyaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
