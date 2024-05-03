acfc1cdf5f1588d86f8b57c3e4492b5d23e31143
MeskourLhoussaine <127257257+MeskourLhoussaine@users.noreply.github.com>
2024-05-03 17:11:38 +0100

    add find Status and update status fonction !!!!

--- chaabiPay/src/main/java/ma/m2t/chaabipay/services/MerchantMethodePaymentService.java
+++ chaabiPay/src/main/java/ma/m2t/chaabipay/services/MerchantMethodePaymentService.java
@@ -1,5 +1,6 @@
 package ma.m2t.chaabipay.services;
 
+import ma.m2t.chaabipay.entites.MerchantMethodePayment;
 import ma.m2t.chaabipay.exceptions.MerchantExceptionNotFound;
 
 import java.util.List;
@@ -8,15 +9,18 @@ import java.util.Map;
 
 public interface MerchantMethodePaymentService {
 
-    //Get/////////////////////
 
     List<Map<String, Object>> getMerchantPaymentMethod(Long merchantId) throws MerchantExceptionNotFound;
-
-
-    ///****************************************************************************************************
-
-//Update/////////////////////
+                /*********----< status de la methode affectue au merchant >----*******/
+                                            /**---#---**/
     void selectPaymentMethodInMerchant(Long merchantId, Long paymentMethodId) throws MerchantExceptionNotFound;
 
-///*************************
+                /**********----< get status de la methode affectue au merchant >----*******/
+                                           /**---#---**/
+    boolean findStatusMerchantPaymentByMerchantIdAndPaymentMethodId(Long merchantId, Long paymentMethodId);
+
+                /**********----------< update status >---------- ****************/
+                                         /**---#---**/
+    void updateMerchantMethodStatusByPaymentMethodId(Long paymentMethodId,Long merchantId,  boolean status) throws MerchantExceptionNotFound;;
+
 }
