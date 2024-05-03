781e1907bdc56ece0d66eb09a96c2eda4ff4024d
MeskourLhoussaine <127257257+MeskourLhoussaine@users.noreply.github.com>
2024-05-03 17:09:09 +0100

    add findByPaymentMethodAndMerchant and findByPaymentMethod "fonctions !!!!"

--- chaabiPay/src/main/java/ma/m2t/chaabipay/repositories/MerchantMethodePaymentRepository.java
+++ chaabiPay/src/main/java/ma/m2t/chaabipay/repositories/MerchantMethodePaymentRepository.java
@@ -1,6 +1,8 @@
 package ma.m2t.chaabipay.repositories;
 
+import ma.m2t.chaabipay.entites.Merchant;
 import ma.m2t.chaabipay.entites.MerchantMethodePayment;
+import ma.m2t.chaabipay.entites.PaymentMethod;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +11,8 @@ public interface MerchantMethodePaymentRepository extends JpaRepository<Merchant
 
     List<MerchantMethodePayment> findByMerchantMerchantId(Long merchantId);
     MerchantMethodePayment findByMerchantMerchantIdAndPaymentMethodPaymentMethodId(Long merchantId, Long paymentMethodId);
+    List<MerchantMethodePayment> findByPaymentMethodAndMerchant(PaymentMethod paymentMethod, Merchant merchant);
+    List<MerchantMethodePayment> findByPaymentMethod(PaymentMethod paymentMethod);
+   // void updateMerchantMethodStatusByPaymentMethodId(Long paymentMethodId);
+
 }
